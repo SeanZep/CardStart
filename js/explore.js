5ba1860430ae5eec4c591d3cd569430ea77e10a2
Karen Ting <kting24@berkeley.edu>
2021-05-01 19:30:01 -0700

    explore decks page - not perfect, but css semi-nicer

--- js/explore.js
+++ js/explore.js
@@ -4,6 +4,34 @@ const list_div = document.querySelector("#about");
 
 db.collection("decks").get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
-        list_div.innerHTML += "<div class='card' id='info1'><h4>" + doc.data().name + "</h4><p>" + doc.data().description + "</p></div>";
+        list_div.innerHTML += "<div class = columns> <div class='cards' id='info1'><p id='title'>" + 
+        doc.data().name + "</p><p>" + doc.data().description + "</p></div></div>";
     });
 });
+
+/*
+var db = firebase.firestore();
+
+const list_div = document.getElementById("#about");
+
+const card_deck = [];
+
+db.collection("decks").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        let card = {};
+        const name = doc.data().name;
+        const description = doc.data().description;
+        card.name = description;
+        card_deck.push(card);
+    });
+});
+
+for (j=0; j<card_deck.length; j*3){
+    const div = document.createElement("div")
+    div.id = "columns"
+for (i=0; i <card_deck.length/3; i++) {
+        let card = card_deck.pop();       
+        div.innerHTML += "<div class = 'cards' id='info1><h4>" + card.name + "</h4><p>" + card.description + "</p></div>";
+} 
+    list_div.appendChild(div)
+}*/
