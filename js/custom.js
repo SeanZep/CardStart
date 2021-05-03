eb9667dd0bc41396b9b5a7fea2a110a06e188df8
Rishi Jain <rishiraij@berkeley.edu>
2021-05-02 21:33:21 -0700

    Fixed small issues with custom page

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,18 @@
 var db = firebase.firestore();
 
-function storeData() {
-    const inputName = document.querySelector("#name_field").value;
-    const inputDesc = document.querySelector("#desc_field").value;
+document.getElementById("submit").addEventListener("click", storeNewCards)
+function storeNewCards() {
+    var inputName = document.querySelector("#name_field").value;
+    var inputDesc = document.querySelector("#desc_field").value;
+    let items = []; 
+    var ques = document.getElementsByClassName('question'); 
+    for (i = 0; i < ques.length; i++) { 
+        items.push(ques[i].value); 
+    }
     db.collection("decks").doc().set({
         name: inputName,
-        description: inputDesc
+        description: inputDesc,
+        questions: items
     }).then(function() {
         console.log("Document successfully written");
     }).catch(function(error) {
@@ -13,26 +20,41 @@ function storeData() {
     });
 }
 
+//document.getElementsByClassName
+
+/*firebase.firestore().collection('decks').doc().set({
+    name: document.querySelector('#name_field').value,
+    description: document.querySelector('#desc_field').value
+}).then(function() {
+    console.log('Document successfully written');
+}).catch(function(error) {
+    console.error('Error writing document: ' + error);
+});*/
+
+//let items = []; var ques = document.getElementsByClassName('question'); for (i = 0; i < ques.length; i++) { items.push(ques[i].value); }
+
+
+
 let count = 2;
 var x = document.getElementById("more");
 x.addEventListener("click", myFunction)
 function myFunction() {
   var box = document.createElement('div');
   box.classList.add('card');
-  box.innerHTML = "<input type='text' placeholder = 'Question" + count +"' id='question'>"
+  box.innerHTML = "<input type='text' placeholder = 'Question " + count +"' class='question'>"
   document.body.appendChild(box); 
   count++;
 }
 
-/*document.getElementById("submit").addEventListener("click", function() {
-    var inputName = document.getElementById("name_field").value;
-    var inputDesc = document.getElementById("desc_field").value;
-    db.collection("decks").doc().set({
+/*document.getElementById('submit').addEventListener('click', function() {
+    var inputName = document.getElementById('name_field').value;
+    var inputDesc = document.getElementById('desc_field').value;
+    db.collection('decks').doc().set({
         name: inputName,
         description: inputDesc
     }).then(function() {
-        console.log("Document successfully written");
+        console.log('Document successfully written');
     }).catch(function(error) {
-        console.error("Error writing document: " + error);
+        console.error('Error writing document: ' + error);
     });
 });*/
